e2031eaa0f818d0175e850d6e7f7cfe0b388c65f
TrustTheProcess712 <marshall1610@hotmail.co.uk>
2023-02-24 12:11:52 +0000

    adds images to json data

--- src/components/ShoppingData.jsx
+++ src/components/ShoppingData.jsx
@@ -16,6 +16,7 @@ const ShoppingData = () => {
         <tr>
           <th>ID</th>
           <th>Item Name</th>
+          <th>Image</th>
           <th>Category</th>
           <th>Description</th>
           <th>Price</th>
@@ -27,6 +28,9 @@ const ShoppingData = () => {
           <tr key={item.id}>
             <td>{item.id}</td>
             <td>{item.title}</td>
+            <td>
+              <img src={item.image} alt={item.title} width={100} />
+            </td>
             <td>{item.category}</td>
             <td>{item.description}</td>
             <td>£{item.price}</td>
